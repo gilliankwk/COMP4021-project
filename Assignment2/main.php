f6db15a85084093af8420470de54ed0a66c2bbc6
Gillian <hillianxd@gmail.com>
2018-05-07 15:24:17 +0800

    Home page icon
    Now can press HKUST icon back to home page

--- Assignment2/main.php
+++ Assignment2/main.php
@@ -103,7 +103,7 @@ $firstname = $users[$_SESSION["username"]]["firstname"];
         });
     </script>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#" id="home">PROF-Profile</a>
+        <a class="navbar-brand" href="#" id="home"><img src = "images/hkust-logo.svg"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
