139dc5bb5ee1b2fcae91e378828a85695e839e54
Gillian <hillianxd@gmail.com>
2018-05-07 16:01:06 +0800

    Add carousel
    Carousel template is done at home page

--- Assignment2/main.php
+++ Assignment2/main.php
@@ -31,7 +31,14 @@ $firstname = $users[$_SESSION["username"]]["firstname"];
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
-    <script>
+    
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	
+	
+	<script>
     $(document).ready(function(){
         $("#signout").on("click",function(){
             var username = getCookie("username");
@@ -146,9 +153,44 @@ $firstname = $users[$_SESSION["username"]]["firstname"];
             <div class="col container" style=":90%">
                 <div class="row">
                     <div class="col p-3 text-center">
-                        <h4>Welcome to PROF-profile</h4>
+                        <h4>Welcome to HKUST Faculty Members' Profile</h4>
                     </div>
                 </div>
+				<div id = "myCarousel" class = "carousel slide" data-ride = "carousel">
+					<!-- Indicators -->
+					<ol class = "carousel-indicators">
+						<li data-target = "#myCarousel" data-slide-to = "0" class = "active"></li>
+						<li data-target = "#myCarousel" data-slide-to = "1"</li>
+						<li data-target = "#myCarousel" data-slide-to = "2"</li>
+					</ol>
+					<!-- Wrapper for slides -->
+					<div class = "carousel-inner">
+						<div class = "carousel-item active">
+							<img src = "images/sun.jpg" alt = "First slide">
+						</div>
+						<div class = "carousel-item">
+							<img src = "images/atrium.jpg" alt = "Second slide">
+						</div>
+						<div class = "carousel-item">
+							<img src = "images/sea.jpg" alt = "Third slide">
+						</div>
+					</div>
+					<!-- Left and right controls -->
+					<a class = "carousel-control-prev" href = "#myCarousel" role="button" data-slide = "prev">
+						<span class = "carousel-control-prev-icon"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class = "carousel-control-next" href = "#myCarousel" role="button" data-slide = "next">
+						<span class = "carousel-control-next-icon"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+				<style>
+				.carousel-inner img {
+					width: 100%;
+					height: 100%;
+				}
+				</style>
             </div>
         </div>
         <!--List--><!--form default all, side bar form, list profile, when click show detail by modal pagination+breadcomb change attribute by breadcomb and form data interchange-->
