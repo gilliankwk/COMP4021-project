1f6b6c4e35d22d23f5e30126279daf4e3ab32795
gilliakwk <hillianxd@gmail.com>
2018-05-08 18:23:42 +0800

    Update full codes on addProf.php
    No debugging yet

--- Assignment2/addProf.php
+++ Assignment2/addProf.php
@@ -12,32 +12,21 @@ $xml->load("Prof.xml");
 $eName = $_POST["add-EnglishName"];
 $cName = $_POST["add-ChineseName"];	// optional
 $title = $_POST["add-head"];
-$sku = $_POST["add-school"];
 $dept = $_POST["add-department"];
-
-$array = array();
-$x = 0;
-while(isset($_POST["area"]) && is_array($_POST["area"])) {
-	$rearea = $_POST["area"][$x];
-	array_push($array, $rearea);
-	$x++;
-}
-print_r($array);
-
+$sku = $_POST["add-school"];
+$area = $_POST["area"];
 $tel = $_POST["add-telephone"];	// optional
 $mail = $_POST["add-email"];
 $page = $_POST["add-homepage"];
-$image = $_FILES["add-image"];
-
-
+$img = $_FILES["add-image"];
 
 function validateFields() {
-	global $xml, $name; 
+	global $xml, $names; 
 	
 	// Check if the name has been taken
     $names = $xml->getElementsByTagName("EnglishName");
     foreach ($names as $node) {
-        if ($node->nodeValue == trim($name)) {
+        if ($node->nodeValue == trim($names)) {
             return "Name already exists!";
         }
     }
@@ -52,7 +41,9 @@ if(error != null) {
 	// Show the error
     echo "<error>" . $error . "</error>";
 }
-else {	
+else {
+	$target = $xml->getElementsByTagName("staffs");
+	
 	// Add new prof
 	$prof = $xml->createDocumentFragment();
 	$nameNode = "<name>";
@@ -62,12 +53,39 @@ else {
 	}
 	$nameNode .= "</name>";
 	
-	$prof->appendXML("");
+	$positions = "<positions>";
+	$positions .= "<position>";
+	$positions .= "<head>$title</head>";
+	$positions .= "<department>$dept</department>";
+	$positions .= "<school>$sku</school>";
+	$positions .= "</position>";
+	$positions .= "</positions>";
+	
+	$researcharea = "<researchAreas>";
+	foreach($area as $node) {
+		if($node != null) {
+			$researcharea .= "<area>$node</area>";
+		}
+	}
+	$researcharea .= "</researchAreas>";
+	
+	$contact = "<contact>";
+	if($tel != null) {
+		$contact .= "<telephone>$tel</telephone>";
+	}
+	$contact .= "<email>$mail</email>";
+	$contact .= "<homepage>$page</homepage>".
+	$contact .= "</contact>";	
+	
+	$image = "<image>$img</image>";
+	move_uploaded_file(img["name"], "data-pic/".trim($eName));
+	
+	$prof->appendXML("<staff>$nameNode $positions $researcharea $contact $image</staff>");
+	$target->appendChild($prof);
 	
 	$xml->save("Prof.xml");
 	
 	// Show success
     echo "<success/>";
 }
-
 ?>
