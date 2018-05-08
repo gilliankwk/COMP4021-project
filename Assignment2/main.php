097aff71e0531c5b193a281543fd5608b9881506
gilliakwk <hillianxd@gmail.com>
2018-05-08 12:51:56 +0800

    Buug fixed for dropdown list
    Boostrap version problem

--- Assignment2/main.php
+++ Assignment2/main.php
@@ -27,9 +27,7 @@ $firstname = $users[$_SESSION["username"]]["firstname"];
     <meta charset="utf-8">
     <meta name="viewport" 
           content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
     
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
